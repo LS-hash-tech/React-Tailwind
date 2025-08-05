40dc65967a9dda2a531620273fa502a1543e0234
Layne Singh <singa0906@icloud.com>
2025-08-05 14:59:35 +0100

    update (new text)

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,17 @@
 function Hero() {
   return (
     <section className="p-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome</h1>
       <img
         src="/assets/palm.jpg"
         alt="Palm Tree"
         className="w-full h-auto rounded-lg shadow-md"
-      />
+      />{" "}
+      <div className="relative z-10 text-black p-10 bg-white rounded-t-4xl">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl font-light mb-4 font-serif">
+          Find your perfect
+          <br /> Travel destination
+        </h2>
+      </div>
     </section>
   );
 }
